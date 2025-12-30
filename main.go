615d2cc44e21a7a16ef8835e2126b0bf318718d9
suisrc <suisrc@outlook.com>
2025-12-30 15:15:49 +0800

    add kwdog

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"strings"
 
+	"github.com/suisrc/zgg/app/kwdog2"
 	"github.com/suisrc/zgg/z"
 	"github.com/suisrc/zgg/z/zc"
 	_ "github.com/suisrc/zgg/ze/rdx"
@@ -35,7 +36,7 @@ func main() {
 	z.Fatal = klog.Fatal
 
 	// front2.Init(www_) // 前端应用，由于需要 wwwFS参数，必须人工初始化
-	// kwdog2.Init() // API边车网关， 通过 Sidecar 模式保护主服务
+	kwdog2.Init() // API边车网关， 通过 Sidecar 模式保护主服务
 	// fluent.Init() // 采集器日志, 为 fluentbit agent 提供 HTTP 收集日志功能
 
 	z.Execute(app, ver, "(https://github.com/suisrc/k8skit) "+app)
