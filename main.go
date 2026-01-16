e080772bd59d43b95cc900280d7879c23ae01d05
suisrc <suisrc@outlook.com>
2026-01-16 17:54:10 +0800

    1.3.10

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"strings"
 
+"github.com/suisrc/zgg/app/kwlog2"
 	"github.com/suisrc/zgg/z"
 	"github.com/suisrc/zgg/z/zc"
 	_ "github.com/suisrc/zgg/z/ze/log/syslog"
@@ -32,7 +33,7 @@ func main() {
 
 	// front2.Init3(www_, "/www", nil) // 前端应用，由于需要 wwwFS参数，必须人工初始化
 	// front2.Init3(os.DirFS("www"), "/", nil) // 前端应用, 使用系统文件夹中文件
-	// kwlog2.Init3(nil) // 采集器日志, 为 fluentbit agent 提供 HTTP 收集日志功能
+	kwlog2.Init3(nil) // 采集器日志, 为 fluentbit agent 提供 HTTP 收集日志功能
 	// kwdog2.Init3(nil) // API反向网关， 通过 Sidecar 模式保护内部服务
 	// proxy2.Init3(nil) // API正向网关， 通过 Sidecar 模式记录外部访问
 
